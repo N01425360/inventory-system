be7cd82c09af0248bcc8145859640621c8de63b9
Dhruv Vinodbhai Patel <n01494614@humber.ca>
2022-03-25 19:05:49 -0400

    Add project files.

---
+++ Project/LogIn.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Project.LogIn
+{
+    public partial class LogIn : Form
+    {
+
+        SqlConnection con = new SqlConnection(@"Data Source=SQLSERVERDEV;Initial Catalog=Inventory;Integrated Security=True");
+
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from registration where username = '"+ textBox1.Text +"' and password = '"+ textBox2.Text  +"'";
+            cmd.ExecuteNonQuery();
+
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+
+            i = Convert.ToInt32(dt.Rows.Count.ToString());
+
+            if(i == 0)
+            {
+                MessageBox.Show("Username and password doesnt match");
+            }
+            else
+            {
+                this.Hide();
+                MDIParent1 mdi = new MDIParent1();
+                mdi.Show();
+            }
+        }
+
+        private void LogIn_Load(object sender, EventArgs e)
+        {
+            if(con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.ResetText();
+            textBox2.ResetText();
+        }
+    }
+}
---
+++ Project/add_new_user.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Project.LogIn
+{
+    public partial class SignUp : Form
+    {
+        private string oldText;
+        SqlConnection con = new SqlConnection(@"Data Source=SQLSERVERDEV;Initial Catalog=Inventory;Integrated Security=True");
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox2.Text.All(chr => char.IsLetter(chr))) //It will checking if user enter is chr or not
+                {
+                    oldText = textBox2.Text;
+                    textBox2.Text = oldText;
+
+                    textBox2.BackColor = System.Drawing.Color.White;
+                    textBox2.ForeColor = System.Drawing.Color.Black;
+                }
+                else
+                {
+                    textBox2.Text = oldText;
+                    textBox2.BackColor = System.Drawing.Color.Red;
+                    textBox2.ForeColor = System.Drawing.Color.White;
+                    MessageBox.Show("Error! Only Enter characters ");  //error message
+                }
+                textBox2.SelectionStart = textBox2.Text.Length;
+            }
+
+
+
+            catch (InvalidOperationException)
+            {
+                MessageBox.Show("Error! invalid operation");
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void SignUp_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text.All(chr => char.IsLetter(chr))) //It will checking if user enter is chr or not
+                {
+                    oldText = textBox1.Text;
+                    textBox1.Text = oldText;
+
+                    textBox1.BackColor = System.Drawing.Color.White;
+                    textBox1.ForeColor = System.Drawing.Color.Black;
+                }
+                else
+                {
+                    textBox1.Text = oldText;
+                    textBox1.BackColor = System.Drawing.Color.Red;
+                    textBox1.ForeColor = System.Drawing.Color.White;
+                    MessageBox.Show("Error! Only Enter characters ");  //error message
+                }
+                textBox1.SelectionStart = textBox1.Text.Length;
+            }
+
+
+
+            catch (InvalidOperationException)
+            {
+                MessageBox.Show("Error! invalid operation");
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+            textBox4.Text = string.Empty;
+            textBox5.Text = string.Empty;
+            textBox6.Text = string.Empty;
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            try //try catch block for any exception in text field
+            {
+                if (textBox6.Text.All(chr => char.IsDigit(chr))) //It will checking if user enter is digit or not
+                {
+                    oldText = textBox6.Text;
+                    textBox6.Text = oldText;
+
+                    textBox6.BackColor = System.Drawing.Color.White;
+                    textBox6.ForeColor = System.Drawing.Color.Black;
+                }
+                else
+                {
+                    textBox6.Text = oldText;
+                    textBox6.BackColor = System.Drawing.Color.Red;
+                    textBox6.ForeColor = System.Drawing.Color.White;
+                    MessageBox.Show("Error! Only Enter Numeric value ");    //error message
+                }
+                textBox6.SelectionStart = textBox6.Text.Length;
+            }
+
+            catch (InvalidOperationException)
+            {
+                MessageBox.Show("Error! invalid operation");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int id;
+            MessageBox.Show(dataGridView1.SelectedCells[0].Value.ToString());
+        }
+
+        private void SignUp_Load_1(object sender, EventArgs e)
+        {
+            if(con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+
+            display();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int i = 0;
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from registration where username='" + textBox3.Text + "'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            i = Convert.ToInt32(dt.Rows.Count.ToString());
+
+            if (i == 0)
+            {
+                SqlCommand cmd1 = con.CreateCommand();
+                cmd1.CommandType = CommandType.Text;
+                cmd1.CommandText = "insert into registration values('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "','" + textBox5.Text + "','" + textBox6.Text + "')";
+                cmd1.ExecuteNonQuery();
+                textBox1.Text = ""; textBox2.Text = ""; textBox3.Text = "";
+                textBox4.Text = ""; textBox5.Text = ""; textBox6.Text = "";
+                display();
+                MessageBox.Show("user records inserted successfully");
+
+            }
+            else
+            {
+                MessageBox.Show("this username already exist");
+            }
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            textBox1.ResetText();
+            textBox2.ResetText();
+            textBox3.ResetText();
+            textBox4.ResetText();
+            textBox5.ResetText();
+            textBox6.ResetText();
+
+        }
+
+        public void display()
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from registration";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            //Deleter function will work here
+
+        }
+    }
+}
